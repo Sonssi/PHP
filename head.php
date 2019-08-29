b35221739badf195ec8c666ac03cb632e17676f2
Lahti Sonja Annika <sonja.lahti@edu.tampere.fi>
2019-08-29 14:06:55 +0300

    First update

---
+++ head.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+    <title>PHP</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="oma.css" type="text/css">
+</head>
+<body>
+    <header>
+        <h1>Sonjan uutissivu</h1>
+    </header>
+<nav>
+    <ul>
+        <li><a href="index.php">Pääsivu</a></li>
+        <li><a href="uutiset.php">Lue uutiset</a></li>
+        <li><a href="uusi_uutinen.php">Lisää uutinen</a></li>
+    </ul>
+</nav>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php require "head.php"; ?>
+
+<h2>Harjoitus 1</h2>
+
+<?php require "footer.php"; ?>
